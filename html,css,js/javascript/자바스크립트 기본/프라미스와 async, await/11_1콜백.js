73bc7e2bbf980cc973ad2fbbb615c181e859d597
zlwmxkdla <zlwmxkdla@naver.com>
2025-03-13 22:46:11 +0900

    2503

--- html,css,js/javascript/자바스크립트 기본/프라미스와 async, await/11_1콜백.js
+++
@@ -1,6 +0,0 @@
-function loadScript(src){
-    let script = document.createElement('script');
-    script.src=src;
-    document.head.append(script);
-}
-loadScript('https://ko.javascript.info/callbacks')
